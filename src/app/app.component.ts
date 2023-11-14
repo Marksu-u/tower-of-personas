1e9d01e15e01198d641eb989c91fb700ee12deaa
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2023-11-14 15:24:31 +0100

    start tower of personnas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tower-of-personas';
+  title = 'Tour of Heroes';
 }
